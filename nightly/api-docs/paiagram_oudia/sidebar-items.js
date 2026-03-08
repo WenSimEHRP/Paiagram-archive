a1773ccb03ff6ef3f3e36a817d53ae5e2c60ad7b
WenSimEHRP <WenSimEHRP@users.noreply.github.com>
2026-03-08 23:02:48 +0000

    deploy: WenSimEHRP/Paiagram@bfb0ddfe9b4e031e7c343325d4c2f088121dc4a3

--- nightly/api-docs/paiagram_oudia/sidebar-items.js
+++ nightly/api-docs/paiagram_oudia/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Direction","PassingMode","Rule","Structure"],"fn":["parse_oud2","parse_oud2_to_ast"],"mod":["write"],"struct":["Diagram","LineMeta","OUD2Parser","Root","Station","TimetableEntry","Train","TrainClass"]};
+window.SIDEBAR_ITEMS = {"enum":["Direction","PassingMode","Rule","Structure"],"fn":["parse_oud2","parse_oud2_to_ast"],"macro":["pair","structure"],"mod":["write"],"struct":["Diagram","LineMeta","OUD2Parser","Root","Station","TimetableEntry","Train","TrainClass"]};
